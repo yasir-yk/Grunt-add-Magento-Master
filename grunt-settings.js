fcfdc7061ce5af9197d2787bc7140f63738dfa65
yasir <chandggreat@gmail.com>
2018-03-07 12:47:27 +0500

    first commit

---
+++ grunt-settings.js
@@ -0,0 +1,18 @@
+module.exports = {
+    sassDir:          'scss/',
+    sassMainFileName: 'styles',
+    cssDir:           'css/',
+    cssMainFileDir:   'css/',
+    cssMainFileName:  'styles',
+    jsDir:            'js/',
+    imgDir:           'images/',
+    imgSourceDir:     'sourceimages/',
+    //hasBower: {includePaths: ['bower_components/foundation/scss']},
+
+    // sftp server
+    sftpServer:       'example.com',
+    sftpPort:         '2121',
+    sftpLogin:        'login',
+    sftpPas:          'password',
+    sftpDestination:  '/pathTo/css'
+  };
